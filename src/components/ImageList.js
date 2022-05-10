1a3f5dcd00a4f3fac8e0feb0606032c2df5bb805
NzrHsp <nazar.hushpit@gmail.com>
2022-05-10 10:04:39 +0300

    add another way to style gallery

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+import "./ImageList.css";
+
+import ImageCard from "./ImageCard";
+
 const ImageList = (props) => {
-  console.log(props);
-  return <div>ImageList</div>;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
